af7af4d3fb385e194dc1559cb549808007782c91
tongchengyu <1429920627@qq.com>
2021-02-25 22:10:55 +0800

    leetcode-701.二叉搜索树中的插入操作

---
+++ src/com/leetcode/code/problemset/Leetcode701.java
@@ -0,0 +1,53 @@
+package com.leetcode.code.problemset;
+
+import com.leetcode.code.pojo.TreeNode;
+
+public class Leetcode701 {
+    /**
+     *
+     * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+     * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+     *
+     *
+     * 示例 1：
+     * 输入：root = [4,2,7,1,3], val = 5
+     * 输出：[4,2,7,1,3,5]
+     * 解释：另一个满足题目要求可以通过的树是：
+     *
+     * 示例 2：
+     * 输入：root = [40,20,60,10,30,50,70], val = 25
+     * 输出：[40,20,60,10,30,50,70,null,null,25]
+     *
+     * 示例 3：
+     * 输入：root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+     * 输出：[4,2,7,1,3,5]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * 解题思路：https://blog.csdn.net/tcy1429920627/article/details/114106705
+     *
+     */
+
+    public static TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) {
+            return new TreeNode(val);
+        }
+        if(root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t = new TreeNode(4,
+                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7));
+
+        System.out.println(insertIntoBST(t, 5));
+
+    }
+}
